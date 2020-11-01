6b9fb16d03eecb392610cbe9b1abea7b1aaef40c
zikuri.codes <zikuri.codes@zikuricodess-MacBook-Pro.local>
2020-11-02 04:46:50 +0700

    commit on 'Master'

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -121,8 +121,8 @@
           <div class="row">
 
             <!-- Area Chart -->
-            <div class="col-xl-8 col-lg-7">
-              <div class="card shadow mb-4">
+            <div class="col-xl-12 col-lg-7">
+              <div class="card shadow mb-">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Total Pengunjung Website</h6>
@@ -149,12 +149,12 @@
             </div>
 
             <!-- Pie Chart -->
-            <div class="col-xl-4 col-lg-5">
-              <div class="card shadow mb-4">
+            <!-- <div class="col-xl-4 col-lg-5">
+              <div class="card shadow mb-4"> -->
                 <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <!-- <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                  <!-- <div class="dropdown no-arrow">
+                  <div class="dropdown no-arrow">
                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
@@ -165,10 +165,10 @@
                       <div class="dropdown-divider"></div>
                       <a class="dropdown-item" href="#">Something else here</a>
                     </div>
-                  </div> -->
-                </div>
+                  </div>
+                </div> -->
                 <!-- Card Body -->
-                <div class="card-body">
+                <!-- <div class="card-body">
                   <div class="chart-pie pt-4 pb-2">
                     <canvas id="myPieChart"></canvas>
                   </div>
@@ -185,7 +185,7 @@
                   </div>
                 </div>
               </div>
-            </div>
+            </div> -->
           </div>
 
           <!-- Content Row -->
